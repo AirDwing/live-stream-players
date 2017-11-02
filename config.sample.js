339462d8e7022fb76b4bad8d16d030bd298d41b3
Willin Wang <willin@willin.org>
2017-11-02 10:15:39 +0800

    init

---
+++ config.sample.js
@@ -0,0 +1,3 @@
+var FLV = 'http://flv-live.jia.360.cn/live_jia_public/_LC_RE_non_3605172938515094868351475112_BX.flv';
+var HLS = 'http://hls-live.jia.360.cn/live_jia_public/_LC_RE_non_3605277091515094857881120781_BX/index.m3u8';
+var RTMP = 'rtmp://xxxx.liveplay.myqcloud.com/live/xxxx';
